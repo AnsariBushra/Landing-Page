3ec02bd4df97008d9cf66f0eb104a12e24c8112d
Bushra <bushraansari.0517@gmail.com>
2022-07-11 13:31:15 +0530

    removes files

--- tut64.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-let text = fs.readFileSync("node.txt","utf-8");
-//text = text.replace("Bushra","Bushra Ansari");
-//console.log("The content of the file is");
-console.log(text);
-
-//console.log("Creating a new file...");
-//fs.writeFileSync("Bushra.txt",text);
